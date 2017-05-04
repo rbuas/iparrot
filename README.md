e3803a4834016dca9a974602fa751e79db44a124
RodrigoBuas <rodrigobuas@gmail.com>
2017-05-04 03:28:49 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# iparrot
+
+A lightweight internationalization module.
+
+## Table of Contents
+
+ * [Installation](#installation)
+ * [Usage](#usage)
+ * [ToDo](#todo)
+ * [License](#license)
+
+## Installation
+
+Installing using npm (node package manager):
+
+    npm install iparrot
+
+Or clone it direct from the repository :
+
+    git clone git://github.com/rbuas/iparrot.git iparrot
+
+## Usage
+
+Require _iparrot_ and start to use the api functions or manipulate objects by own methods.
+
+```javascript
+var iparrot = require('iparrot');
+
+//TODO
+```
+
+## ToDo
+
+There are a lot of things still to be done and to be made better. If you have any special requests please open an issue with a feature request.
+
+## License
+
+_iparrot_ is licensed under the MIT License. (See LICENSE)
---
+++ iparrot.js
@@ -0,0 +1,50 @@
+module.exports = IParrot;
+
+var fs = require("fs");
+var path = require("path");
+var jsext = require("jsext");
+
+function IParrot (options) {
+    var self = this;
+    self.options = Object.assign(IParrot.DEFAULTOPTIONS, options) || {};
+    self.cache = {};
+}
+
+IParrot.DEFAULTOPTIONS = {
+    languages : ["EN","PT","FR","ES"],
+    path : "./resources",
+    filename : "messages.json"
+};
+
+IParrot.prototype.resetcache = function() {
+    self.cache = {};
+}
+
+IParrot.prototype.text = function(t, lang) {
+    var self = this;
+    if(!t) return;
+
+    lang = lang || self.options.languages && self.options.languages.length && self.options.languages[0];
+    if(typeof(t) == "object")
+        return t[lang] || t[t.first()];
+
+    var dic = self.dictionary(lang);
+    return dic[t] || t;
+}
+
+IParrot.prototype.dictionary = function(lang) {
+    var self = this;
+    if(!lang) return;
+
+    if(self.cache[lang]) return self.cache[lang];
+
+    var ifile = path.normalize(path.join(self.options.path, lang.toLowerCase(), self.options.filename));
+    if(!jsext.fileExists(ifile)) {
+        console.log("IPARROT : can not found the file", ifile);
+        return;
+    }
+
+    var dic = jsext.loadJsonFile(ifile) || {};
+    self.cache[lang] = dic;
+    return self.cache[lang];
+}
